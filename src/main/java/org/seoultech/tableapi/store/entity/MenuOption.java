a7ce97c5d5259135595253dcc03bce86b25f7e38
orieasy1 <easy1nhard2@gmail.com>
2024-11-10 18:58:37 +0900

    Fix: Entity에 빠진 부분과 잘못된 부분이 있어서 수정

--- src/main/java/org/seoultech/tableapi/store/entity/MenuOption.java
+++ src/main/java/org/seoultech/tableapi/store/entity/MenuOption.java
@@ -2,6 +2,7 @@ package org.seoultech.tableapi.store.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.seoultech.tableapi.common.entity.BaseEntity;
@@ -17,7 +18,7 @@ public class MenuOption extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "menu_id", nullable = false)
-    private Menu menuId;
+    private Menu menu;
 
     @Column(name = "option_name", nullable = false)
     private String optionName;
@@ -27,4 +28,12 @@ public class MenuOption extends BaseEntity {
 
     @Column(name = "option_available", nullable = false)
     private Boolean optionAvailable;
+
+    @Builder
+    public MenuOption(Menu menu, String optionName, Integer price, Boolean optionAvailable) {
+        this.menu = menu;
+        this.optionName = optionName;
+        this.price = price;
+        this.optionAvailable = optionAvailable;
+    }
 }
